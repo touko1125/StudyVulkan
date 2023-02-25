8b0a3650a914eddaa6aa649da592fbef4c1f1969
touko1125 <s2110642@s.tsukuba.ac.jp>
2023-02-25 21:12:50 +0900

    add:ウィンドウの初期化

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -11,24 +11,46 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 class HelloTriangleApplication {
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 private:
+    GLFWwindow* window;
+    void initWindow() {
+        glfwInit();
+        
+        //間に何のAPIも挟まずサイズ変更不可能な状態でGLFWを呼び出す
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        
+        //ウィンドウの作成
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+    
     void initVulkan() {
         
     }
     
     void mainLoop() {
-        
+        while(!glfwWindowShouldClose(window)){
+            glfwPollEvents();
+        }
     }
     
     void cleanup() {
+        glfwDestroyWindow(window);
         
+        glfwTerminate();
     }
 };
 
