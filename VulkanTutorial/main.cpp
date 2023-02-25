74dbcc8b84ca3ac9a1028adda400138d2d3ef05b
touko1125 <s2110642@s.tsukuba.ac.jp>
2023-02-25 23:20:02 +0900

    add:論理デバイスの追加、うまくいっていない？

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -57,6 +57,8 @@ private:
     VkInstance instance;
     VkDebugUtilsMessengerEXT debugMessanger;
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device;
+    VkQueue graphicsQueue;
     void initWindow() {
         glfwInit();
         
@@ -72,6 +74,7 @@ private:
         createInstance();
         setupDebugMessenger();
         pickPhysicalDevice();
+        createLogicalDevice();
     }
     
     static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
@@ -260,6 +263,41 @@ private:
         }
     }
     
+    void createLogicalDevice() {
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+        
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount = 1;
+        
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+        
+        VkPhysicalDeviceFeatures deviceFeatures{};
+        
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = 1;
+        createInfo.pEnabledFeatures = &deviceFeatures;
+
+        createInfo.enabledExtensionCount = 0;
+        
+        if(enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
+        
+        if(vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create logical device!");
+        }
+        
+        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+    }
+    
     void mainLoop() {
         while(!glfwWindowShouldClose(window)){
             glfwPollEvents();
@@ -270,6 +308,8 @@ private:
         if(enableValidationLayers) {
             DestroyDebugUtilsMessengerEXT(instance, debugMessanger, nullptr);
         }
+        
+        vkDestroyDevice(device, nullptr);
         
         vkDestroyInstance(instance, nullptr);
         
