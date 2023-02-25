c5426fadd30c094bf1745c28c1a1ec4304054c2f
touko1125 <s2110642@s.tsukuba.ac.jp>
2023-02-25 20:32:35 +0900

    create project

---
+++ VulkanTutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VulkanTutorial
+//
+//  Created by 島田東子 on 2023/02/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
