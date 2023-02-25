0b0b44fa2a1f2c5225ff5d634d52c35a677d4e90
touko1125 <s2110642@s.tsukuba.ac.jp>
2023-02-25 22:58:29 +0900

    add:物理デバイスの要件にキューファミリーの追加

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -11,6 +11,7 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <vector>
+#include <optional>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -199,8 +200,41 @@ private:
         }
     }
     
+    struct QueueFamilyIndices {
+        std::optional<uint32_t> graphicsFamily;
+        
+        bool isComplete() {
+            return graphicsFamily.has_value();
+        }
+    };
+    
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+        QueueFamilyIndices indices;
+        
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+        
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+        
+        int i = 0;
+        for(const auto& queueFamily : queueFamilies) {
+            if(queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                indices.graphicsFamily = i;
+            }
+            
+            if(indices.isComplete()) break;
+            
+            i++;
+        }
+        
+        return indices;
+    }
+    
     bool isDeviceSuitable(VkPhysicalDevice device) {
-        return true;
+        QueueFamilyIndices indices = findQueueFamilies(device);
+        
+        return indices.isComplete();
     }
     
     void pickPhysicalDevice() {
