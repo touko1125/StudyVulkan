e772a4fe3b2a508971d67df08dd3734a4d2a7065
touko1125 <s2110642@s.tsukuba.ac.jp>
2023-02-25 21:29:13 +0900

    add:glfw extension

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -26,6 +27,7 @@ public:
     }
 private:
     GLFWwindow* window;
+    VkInstance instance;
     void initWindow() {
         glfwInit();
         
@@ -38,7 +40,44 @@ private:
     }
     
     void initVulkan() {
+        createInstance();
+    }
+    
+    void createInstance() {
+        //アプリケーション情報を定める構造体
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Vulkan Tutorial";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
         
+        //Vulkanインスタンスを作成するための構造体
+        VkInstanceCreateInfo createInfo;
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        
+        std::vector<const char*> requiredExtensions;
+        for(uint32_t i = 0;i<glfwExtensionCount;i++) {
+            requiredExtensions.emplace_back(glfwExtensions[i]);
+        }
+        requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+        
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+        
+        createInfo.enabledExtensionCount = (uint32_t)requiredExtensions.size();
+        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+        
+        createInfo.enabledLayerCount = 0;
+        
+        if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
     }
     
     void mainLoop() {
