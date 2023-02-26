a9374aac346533a762a8123bc2f118495773d054
touko1125 <s2110642@s.tsukuba.ac.jp>
2023-02-26 23:06:51 +0900

    add:イメージビューの追加

--- VulkanTutorial/main.cpp
+++ VulkanTutorial/main.cpp
@@ -70,6 +70,10 @@ private:
     VkQueue graphicsQueue,presentQueue;
     VkSurfaceKHR surface;
     VkSwapchainKHR swapChain;
+    std::vector<VkImage> swapChainImages;
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
+    std::vector<VkImageView> swapChainImageViews;
     void initWindow() {
         glfwInit();
         
@@ -88,6 +92,7 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
     }
     
     static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
@@ -368,6 +373,13 @@ private:
         if(vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapChain) != VK_SUCCESS) {
             throw std::runtime_error("failed to create swap chain!");
         }
+        
+        vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
+        swapChainImages.resize(imageCount);
+        vkGetSwapchainImagesKHR(device, swapChain, &imageCount, swapChainImages.data());
+        
+        swapChainImageFormat = surfaceFormat.format;
+        swapChainExtent = extent;
     }
     
     bool checkDeviceExtensionSupport(VkPhysicalDevice device) {
@@ -471,6 +483,31 @@ private:
         }
     }
     
+    void createImageViews() {
+        swapChainImageViews.resize(swapChainImages.size());
+        
+        for(size_t i = 0; i < swapChainImages.size(); i++) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+            
+            if(vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create image views!");
+            }
+        }
+    }
+    
     void mainLoop() {
         while(!glfwWindowShouldClose(window)){
             glfwPollEvents();
@@ -481,6 +518,10 @@ private:
         if(enableValidationLayers) {
             DestroyDebugUtilsMessengerEXT(instance, debugMessanger, nullptr);
         }
+        for(auto imageView : swapChainImageViews) {
+            vkDestroyImageView(device, imageView, nullptr);
+        }
+        
         vkDestroySwapchainKHR(device, swapChain, nullptr);
         
         vkDestroyDevice(device, nullptr);
